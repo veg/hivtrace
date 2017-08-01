72c55f74f1b551f5b5248e40f305993f85f3a330
Steven Weaver <sweaver@temple.edu>
2017-08-01 10:08:12 -0400

    bumping version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def setup_package():
 
     setup(
         name='hivtrace',
-        version='0.3.1',
+        version='0.3.2',
         description='HIV-TRACE',
         author='Steven Weaver',
         author_email='steven@stevenweaver.org',
